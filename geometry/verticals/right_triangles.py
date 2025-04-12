7c816b4d07aa130256b1ad4bb53786a2188d8a3f
StevenGolovkine <steven_golovkine@icloud.com>
2025-04-12 15:27:48 -0400

    Bissect right angle

---
+++ geometry/verticals/right_triangles.py
@@ -0,0 +1,111 @@
+"""
+Visual proof of the internal bisector of the right angle of a right triangle bisects
+the square of the hypotenuse.
+Proofs without Words I. Roger B. Nelsen. p. 16.
+"""
+import numpy as np
+
+from manim import MovingCameraScene
+from manim import Create, Uncreate, Write, Transform, TransformFromCopy
+from manim import VGroup, FadeIn, FadeOut , FunctionGraph
+from manim import DashedVMobject, Line, Point, Polygon, RoundedRectangle
+from manim import Text, Tex
+
+from manim import config
+from manim import ORIGIN, LEFT, RIGHT, DOWN, LIGHT, UP, PI
+
+# COLORS
+BLUE = "#B0E1FA"
+VIOLET = "#E8C9FA"
+RED = "#F79BC5"
+GREEN = "#DBF9E7"
+YELLOW = "#EFE9B7"
+ORANGE = "#F6CCB0"
+BLACK = "#000000"
+WHITE = "#F4EDDE"
+
+# Make it vertical
+SCALE_FACTOR = 1
+# Flip width => height, height => width
+tmp_pixel_height = config.pixel_height
+config.pixel_height = config.pixel_width
+config.pixel_width = tmp_pixel_height
+# Change coord system dimensions
+config.frame_height = config.frame_height / SCALE_FACTOR
+config.frame_width = config.frame_height * 9 / 16
+
+
+class Triangle(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        self.camera.frame.save_state()
+
+        txt_copy = Text(
+            r"@chill.maths", font_size=12,
+            font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        ).to_edge(RIGHT + DOWN, buff=0.1)
+        self.add(txt_copy)
+
+        # Introduction text
+        # La bissectrice de l'angle droit d'un triangle rectangle coupe le carré 
+        # construit sur l'hypothénuse en deux moitiés identiques.
+        txt_title = [
+            Tex(r"La bissectrice de", font_size=32, color=BLACK),
+            Tex(r"l'angle droit d'un triangle", font_size=32, color=BLACK),
+            Tex(r"rectangle coupe le carré", font_size=32, color=BLACK),
+            Tex(r"construit sur l'hypothénuse",font_size=32, color=BLACK),
+            Tex(r"en deux moitiés identiques.",font_size=32, color=BLACK),
+        ]
+        txt_title = VGroup(*txt_title).arrange(DOWN).move_to([0, 2, 0])
+
+        txt = [
+            Tex(r"Démonstration", font_size=36, color=BLACK),
+            Tex(r"Roland H. Eddy", font_size=28, color=BLACK)
+        ]
+        txt = VGroup(*txt).arrange(DOWN)
+
+        self.add(
+            txt_title,
+            txt
+        )
+        self.wait(1)
+        self.play(
+            Uncreate(txt_title),
+            Uncreate(txt)
+        )
+
+
+        # Finish
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
+        # Logo
+        ref = [
+            Tex(r"College Mathematics Journal,", font_size=30, color=BLACK),
+            Tex(r"vol. 22, no. 5 (Nov. 1991),", font_size=30, color=BLACK),
+            Tex(r"p. 420", font_size=30, color=BLACK),
+        ]
+        ref = VGroup(*ref)\
+            .arrange(DOWN, aligned_edge=LEFT, center=False, buff=0.1)\
+            .move_to([0, 2, 0])
+        
+        self.play(Write(ref))
+
+        text = Text(
+            "chill.maths", font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        )
+        # Ajouter un élément mathématique, par exemple une sinusoïde
+        sine_wave = FunctionGraph(
+            lambda x: 0.1 * np.sin(2 * np.pi * x),
+            x_range=[-3, 3],
+            color=BLACK
+        )
+        sine_wave.next_to(text, DOWN, buff=0.2)
+        
+        self.play(
+            FadeIn(text, scale=0.5),
+            Create(sine_wave),
+            run_time=2
+        )
+
+        self.wait(1)
