18f15d5039138518cf3f75b2e3fb3af670a151be
StevenGolovkine <steven_golovkine@icloud.com>
2025-04-13 15:51:10 -0400

    Bissect right angle

--- geometry/verticals/right_triangles.py
+++ geometry/verticals/right_triangles.py
@@ -6,13 +6,13 @@ Proofs without Words I. Roger B. Nelsen. p. 16.
 import numpy as np
 
 from manim import MovingCameraScene
-from manim import Create, Uncreate, Write, Transform, TransformFromCopy
-from manim import VGroup, FadeIn, FadeOut , FunctionGraph
-from manim import DashedVMobject, Line, Point, Polygon, RoundedRectangle
-from manim import Text, Tex
+from manim import Create, Uncreate, Write, FadeTransform, TransformFromCopy
+from manim import VGroup, FadeIn, FadeOut , FunctionGraph, Rotate
+from manim import Line, Point, Polygon, RoundedRectangle, Square, Angle
+from manim import Text, Tex, Intersection
 
 from manim import config
-from manim import ORIGIN, LEFT, RIGHT, DOWN, LIGHT, UP, PI
+from manim import ORIGIN, LEFT, RIGHT, DOWN, LIGHT, UP, PI, DEGREES
 
 # COLORS
 BLUE = "#B0E1FA"
@@ -33,6 +33,19 @@ config.pixel_width = tmp_pixel_height
 # Change coord system dimensions
 config.frame_height = config.frame_height / SCALE_FACTOR
 config.frame_width = config.frame_height * 9 / 16
+
+
+def get_vertices(obj: Polygon) -> list[Line]:
+    vertices = obj.get_vertices()
+    coords_vertices = []
+    for i in range(len(vertices)):
+        if i < len(vertices)-1:
+            p1, p2 = [vertices[i], vertices[i + 1]]
+        else:
+            p1, p2 = [vertices[-1], vertices[0]]
+        guide_line = Line(p1, p2)
+        coords_vertices.append(guide_line)
+    return coords_vertices
 
 
 class Triangle(MovingCameraScene):
@@ -74,6 +87,127 @@ class Triangle(MovingCameraScene):
             Uncreate(txt)
         )
 
+        # Triangle
+        triangle_b = Polygon(
+            [-1, -0.75, 0], [1, -0.75, 0], [1, 0.75, 0],
+            stroke_width=2,
+            color=BLACK, fill_color=BLUE, fill_opacity=1
+        ).move_to([0, 1.5, 0])
+
+        self.play(
+            Create(triangle_b)
+        )
+
+        self.play(
+            Rotate(triangle_b, 143 * DEGREES, about_point=[0, 1.5, 0]),
+        )
+
+        coords_vertices_b = get_vertices(triangle_b)
+
+        sq_c2 = Square(side_length=2.5, stroke_color=BLACK, stroke_width=2)\
+            .move_to(coords_vertices_b[2], UP)
+        self.play(
+            FadeTransform(coords_vertices_b[2].set_color(WHITE), sq_c2, stretch=True),
+        )
+
+        # Complete other triangles
+        triangle_b_c = triangle_b.copy()
+        self.play(
+            triangle_b_c.animate.rotate(PI).next_to(sq_c2, DOWN, buff=0),
+        )
+
+        triangle_b_c2 = triangle_b.copy()
+        self.play(
+            triangle_b_c2.animate.rotate(PI/2).next_to(sq_c2, LEFT, buff=0),
+        )
+
+        triangle_b_c3 = triangle_b.copy()
+        self.play(
+            triangle_b_c3.animate.rotate(-PI/2).next_to(sq_c2, RIGHT, buff=0),
+        )
+
+        # Bisector
+        bisector = Line(
+            triangle_b.get_vertices()[1], triangle_b_c.get_vertices()[1],
+            stroke_width=2,
+            color=RED
+        )
+        self.play(
+            Create(bisector)
+        )
+
+        # Add angles
+        angle_a = Angle(
+            bisector, coords_vertices_b[1],
+            color=BLACK, other_angle=True, stroke_width=1
+        )
+        angle_b = Angle(
+            coords_vertices_b[0], bisector, quadrant=(-1, 1), radius=0.25,
+            color=BLACK, other_angle=True, stroke_width=1
+        )
+        txt_pi = Tex(r"$\frac{\pi}{2}$", font_size=28, color=BLACK).\
+            next_to(angle_a, DOWN, buff=0.1)
+        txt_pi_2 = txt_pi.copy().next_to(angle_b, DOWN + 0.5 * RIGHT, buff=0.1)
+        self.play(
+            Create(angle_a),
+            Create(angle_b),
+            Write(txt_pi),
+            Write(txt_pi_2),
+        )
+
+        coords_vertices_b_c = get_vertices(triangle_b_c)
+        angle_c = Angle(
+            bisector, coords_vertices_b_c[1],
+            color=BLACK, other_angle=True, stroke_width=1,
+            quadrant=(-1, 1)
+        )
+        angle_d = Angle(
+            coords_vertices_b_c[0], bisector, quadrant=(-1, -1), radius=0.25,
+            color=BLACK, other_angle=True, stroke_width=1
+        )
+        txt_pi_3 = txt_pi.copy().next_to(angle_c, UP, buff=0.1)
+        txt_pi_4 = txt_pi.copy().next_to(angle_d, UP + 0.5 * LEFT, buff=0.1)
+        self.play(
+            Create(angle_c),
+            Create(angle_d),
+            Write(txt_pi_3),
+            Write(txt_pi_4)
+        )
+
+        # Color side of the square
+        A = Line(
+            triangle_b.get_vertices()[0],
+            [-0.18, 1.49716161, 0],
+            stroke_width=8,
+            color = ORANGE
+        )
+        B = Line(
+            triangle_b_c.get_vertices()[0],
+            [0.18, -0.99716161, 0],
+            stroke_width=8,
+            color = ORANGE
+        )
+        self.play( 
+            Create(A),
+            Create(B)
+        )
+
+        C = Line(
+            triangle_b.get_vertices()[2],
+            [-0.18, 1.49716161, 0],
+            stroke_width=8,
+            color = VIOLET
+        )
+        D = Line(
+            triangle_b_c.get_vertices()[2],
+            [0.18, -0.99716161, 0],
+            stroke_width=8,
+            color = VIOLET
+        )
+        self.play( 
+            Create(C),
+            Create(D)
+        )
 
         # Finish
         self.wait(2)
