0884788385cd9481f4ca3de9c301db81551b1043
StevenGolovkine <steven_golovkine@icloud.com>
2025-05-04 12:04:40 -0400

    Algebric inequality

---
+++ inequalities/verticals/algebric_inequality_II.py
@@ -0,0 +1,118 @@
+"""
+Visual proof of the arithmetic mean - geometric mean inequality for three positive
+numbers.
+Proofs without Words II. Roger B. Nelsen. p. 74.
+"""
+import numpy as np
+
+from manim import MovingCameraScene
+from manim import Create, Uncreate, Write, Transform, TransformFromCopy
+from manim import VGroup, FadeIn, FadeOut, FunctionGraph, Rectangle, RoundedRectangle
+from manim import Text, Tex
+
+from manim import config
+from manim import LEFT, RIGHT, DOWN, LIGHT, UP
+
+# COLORS
+BLUE = "#B0E1FA"
+VIOLET = "#E8C9FA"
+RED = "#F79BC5"
+GREEN = "#DBF9E7"
+YELLOW = "#EFE9B7"
+ORANGE = "#F6CCB0"
+BLACK = "#000000"
+WHITE = "#F4EDDE"
+
+# Make it vertical
+SCALE_FACTOR = 1
+# Flip width => height, height => width
+tmp_pixel_height = config.pixel_height
+config.pixel_height = config.pixel_width
+config.pixel_width = tmp_pixel_height
+# Change coord system dimensions
+config.frame_height = config.frame_height / SCALE_FACTOR
+config.frame_width = config.frame_height * 9 / 16
+
+
+class Proof(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        self.camera.frame.save_state()
+
+        txt_copy = Text(
+            r"@chill.maths", font_size=12,
+            font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        ).to_edge(RIGHT + DOWN, buff=0.1)
+        self.add(txt_copy)
+
+        # Introduction text
+        txt_title = [
+            Tex(r"Une inégalité", font_size=48, color=BLACK),
+            Tex(r"algébrique", font_size=48, color=BLACK),
+        ]
+        txt_title = VGroup(*txt_title).arrange(DOWN).move_to([0, 2, 0])
+
+        txt = [
+            Tex(r"Démonstration", font_size=36, color=BLACK),
+            Tex(r"Wei-Dong Jiang", font_size=28, color=BLACK)
+        ]
+        txt = VGroup(*txt).arrange(DOWN)
+
+        self.add(
+            txt_title,
+            txt
+        )
+        self.wait(1)
+        self.play(
+            Uncreate(txt_title),
+            Uncreate(txt)
+        )
+
+        # Text
+        txt = [
+            Tex(r"Soit $a \geq b \geq c \geq 0$,", font_size=30, color=BLACK),
+            Tex(r"et soit $a + b + c \leq 1$,", font_size=30, color=BLACK),
+            Tex(r"alors $a^2 + 3b^2 + 5c^2 \leq 1$.", font_size=30, color=BLACK),
+        ]
+        txt = VGroup(*txt).arrange(DOWN, aligned_edge=LEFT).move_to([0, 2.5, 0])
+
+        self.play(
+            Write(txt),
+        )
+
+
+        # Finish
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
+        # Logo
+        ref = [
+            Tex(r"Problem 12, 1989 Leningrad", font_size=26, color=BLACK),
+            Tex(r"Mathematics Olympiad, Grade 7.", font_size=26, color=BLACK),
+            Tex(r"Mathematics Magazine, vol. 80,", font_size=26, color=BLACK),
+            Tex(r"no. 5 (Dec. 2007), p.344", font_size=26, color=BLACK)
+        ]
+        ref = VGroup(*ref)\
+            .arrange(DOWN, aligned_edge=LEFT, center=False, buff=0.1)\
+            .move_to([0, 2, 0])
+
+        self.play(Write(ref))
+
+        text = Text(
+            "chill.maths", font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        )
+        # Ajouter un élément mathématique, par exemple une sinusoïde
+        sine_wave = FunctionGraph(
+            lambda x: 0.1 * np.sin(2 * np.pi * x),
+            x_range=[-3, 3],
+            color=BLACK
+        )
+        sine_wave.next_to(text, DOWN, buff=0.2)
+        
+        self.play(
+            FadeIn(text, scale=0.5),
+            Create(sine_wave),
+            run_time=2
+        )
+
+        self.wait(1)
