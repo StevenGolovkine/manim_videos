e4f31d69b89bdc1c108ddb1271cf48535f73ab24
StevenGolovkine <steven_golovkine@icloud.com>
2025-06-29 11:22:45 -0400

    Sums of products of consecutives integers

--- integers_sum/verticals/sums_products_integer.py
+++ integers_sum/verticals/sums_products_integer.py
@@ -7,7 +7,7 @@ import numpy as np
 from manim import MovingCameraScene
 from manim import Create, Uncreate, Write
 from manim import Brace, VGroup, FadeIn, FadeOut, FunctionGraph, Rotate
-from manim import Text, Tex, Square, RoundedRectangle, Transform
+from manim import Text, Tex, Rectangle, RoundedRectangle, Transform
 
 from manim import config
 from manim import LEFT, RIGHT, DOWN, LIGHT, UP, PI
@@ -78,6 +78,107 @@ class Sums(MovingCameraScene):
         )
         self.wait(0.5)
 
+
+        # Rectangle
+        rect = Rectangle(
+            width=3, height=2, color=BLACK, fill_color=WHITE, fill_opacity=1,
+            stroke_width=2
+        ).move_to([0, 1.5, 0])
+        txt_1 = Tex(
+            r"$1 + 2 + \cdots + (k + 1) + (k + 2)$", font_size=20, color=BLACK
+        ).next_to(rect, UP, buff=0.1)
+        txt_2 = Tex(r"$1 + 2 + \cdots + k$", font_size=20, color=BLACK).\
+            rotate(PI / 2).\
+            next_to(rect, LEFT, buff=0.1)
+
+        self.play(
+            Create(rect),
+            Write(txt_1),
+            Write(txt_2),
+            run_time=1
+        )
+
+        # Inside rectangle
+        inside_rect1 = Rectangle(
+            width=2, height=1, color=RED, fill_color=RED, fill_opacity=0.5,
+            stroke_width=0
+        ).align_to(rect, LEFT + DOWN)
+        txt_inside1 = Tex(
+            r"$1 + 2 + \cdots + k + (k +1 )$", font_size=15, color=BLACK
+        ).next_to(inside_rect1, UP, buff=0.1)
+        txt_inside11 = Tex(
+            r"$k$", font_size=15, color=BLACK
+        ).next_to(inside_rect1, LEFT, buff=-0.15)
+
+        self.play(
+            Create(inside_rect1),
+            Write(txt_inside1),
+            Write(txt_inside11),
+        )
+
+        inside_rect2 = Rectangle(
+            width=1, height=2, color=BLUE, fill_color=BLUE, fill_opacity=0.5,
+            stroke_width=0
+        ).align_to(rect, RIGHT + DOWN)
+        txt_inside2 = Tex(
+            r"$k + 2$", font_size=15, color=BLACK
+        ).next_to(inside_rect2, UP, buff=-0.2)
+
+        self.play(
+            Create(inside_rect2),
+            Write(txt_inside2)
+        )
+
+
+        # Second rectangle
+        rect2 = Rectangle(
+            width=3, height=2, color=BLACK, fill_color=WHITE, fill_opacity=1,
+            stroke_width=2
+        ).move_to([0, -1.5, 0])
+        txt_3 = Tex(
+            r"$1 + 2 + \cdots + (n + 1) + (n + 2)$", font_size=20, color=BLACK
+        ).next_to(rect2, UP, buff=0.1)
+        txt_4 = Tex(r"$1 + 2 + \cdots + n$", font_size=20, color=BLACK).\
+            rotate(PI / 2).\
+            next_to(rect2, LEFT, buff=0.1)
+
+        self.play(
+            Create(rect2),
+            Write(txt_3),
+            Write(txt_4),
+            run_time=1
+        )
+
+        # Inside rectangle
+        inside_rect3 = Rectangle(
+            width=1, height=0.25, color=RED, fill_color=RED, fill_opacity=0.5,
+            stroke_width=0
+        ).align_to(rect2, LEFT + UP)
+        txt_5 = Tex(
+            r"$1 \times 2 \times 3$", font_size=15, color=BLACK
+        ).move_to(inside_rect3.get_center_of_mass())
+        self.play(
+            Create(inside_rect3),
+            Write(txt_5)
+        )
+
+        inside_rect7 = Rectangle(
+            width=2.25, height=0.75, color=RED, fill_color=RED, fill_opacity=0.5,
+            stroke_width=0
+        ).align_to(rect2, LEFT + DOWN)
+        inside_rect8 = Rectangle(
+            width=0.75, height=2, color=RED, fill_color=RED, fill_opacity=0.5,
+            stroke_width=0
+        ).align_to(rect2, RIGHT + DOWN)
+        inside_rect_n = VGroup(inside_rect7, inside_rect8)
+        txt_n = Tex(
+            r"$n(n + 1)(n + 2)$", font_size=15, color=BLACK
+        ).move_to(inside_rect7.get_center_of_mass() + [0.75, 0, 0])
+
+        self.play(
+            Create(inside_rect_n),
+            Write(txt_n)
+        )
 
         # Finish
         self.wait(2)
