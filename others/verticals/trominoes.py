5b5ecece03da556b8892ee7c633e85511027edfb
StevenGolovkine <steven_golovkine@icloud.com>
2025-09-20 11:09:34 -0400

    Tiling triominoes

---
+++ others/verticals/trominoes.py
@@ -0,0 +1,106 @@
+"""
+Visual proof of Tiling with trominoes.
+Proofs without Words II. Roger B. Nelsen. p. 123.
+"""
+import numpy as np
+
+from manim import MovingCameraScene, Scene, ManimColor
+from manim import Create, Uncreate, Write, Transform, TransformFromCopy
+from manim import VGroup, FadeIn, FadeOut , FunctionGraph, Rotate
+from manim import Line, Point, Polygon, RoundedRectangle, Circle, Angle
+from manim import line_intersection, DashedLine, RightAngle
+from manim import Text, Tex, Intersection, LaggedStart
+
+from manim import config
+from manim import ORIGIN, LEFT, RIGHT, DOWN, LIGHT, UP, PI, DEGREES
+
+# COLORS
+BLUE = "#B0E1FA"
+VIOLET = "#E8C9FA"
+RED = "#F79BC5"
+GREEN = "#DBF9E7"
+YELLOW = "#EFE9B7"
+ORANGE = "#F6CCB0"
+BLACK = "#000000"
+WHITE = "#F4EDDE"
+
+# Make it vertical
+SCALE_FACTOR = 1
+# Flip width => height, height => width
+tmp_pixel_height = config.pixel_height
+config.pixel_height = config.pixel_width
+config.pixel_width = tmp_pixel_height
+# Change coord system dimensions
+config.frame_height = config.frame_height / SCALE_FACTOR
+config.frame_width = config.frame_height * 9 / 16
+
+
+class Trio(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        self.camera.frame.save_state()
+
+        txt_copy = Text(
+            r"@chill.maths", font_size=12,
+            font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        ).to_edge(RIGHT + DOWN, buff=0.1)
+        self.add(txt_copy)
+
+        # Introduction text
+        txt_title = [
+            Tex(r"Pavage avec ", font_size=48, color=BLACK),
+            Tex(r"des triominos", font_size=48, color=BLACK),
+        ]
+        txt_title = VGroup(*txt_title).arrange(DOWN).move_to([0, 2, 0])
+
+        txt = [
+            Tex(r"Démonstration", font_size=36, color=BLACK),
+            Tex(r"Solomon W. Golomb", font_size=28, color=BLACK)
+        ]
+        txt = VGroup(*txt).arrange(DOWN)
+
+        self.add(
+            txt_title,
+            txt
+        )
+        self.wait(1)
+        self.play(
+            Uncreate(txt_title),
+            Uncreate(txt)
+        )
+
+
+        # Finish
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
+        # Logo
+        ref = [
+            Tex(r"American Mathematical", font_size=30, color=BLACK),
+            Tex(r"Monthly, vol. 61, no. 10,", font_size=30, color=BLACK),
+            Tex(r"(Dec. 1954), pp. 675-682", font_size=30, color=BLACK),
+        ]
+        ref = VGroup(*ref)\
+            .arrange(DOWN, aligned_edge=LEFT, center=False, buff=0.1)\
+            .move_to([0, 2, 0])
+        
+        self.play(Write(ref))
+
+        text = Text(
+            "chill.maths", font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        )
+        # Ajouter un élément mathématique, par exemple une sinusoïde
+        sine_wave = FunctionGraph(
+            lambda x: 0.1 * np.sin(2 * np.pi * x),
+            x_range=[-3, 3],
+            color=BLACK
+        )
+        sine_wave.next_to(text, DOWN, buff=0.2)
+        
+        self.play(
+            FadeIn(text, scale=0.5),
+            Create(sine_wave),
+            run_time=2
+        )
+
+        self.wait(1)
