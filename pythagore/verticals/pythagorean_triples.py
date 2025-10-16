8158bc07088453aae95647b00b10ea80aa37604c
StevenGolovkine <steven_golovkine@icloud.com>
2025-10-16 09:40:30 -0400

    Pythagorean triples

---
+++ pythagore/verticals/pythagorean_triples.py
@@ -0,0 +1,131 @@
+"""
+Visual proof of the Pythagorean triples via double angle formulas.
+Proofs without Words I. Roger B. Nelsen. p. 141.
+"""
+import numpy as np
+
+from manim import MovingCameraScene, Mobject
+from manim import Brace, Line, Polygon
+from manim import RoundedRectangle, Square
+from manim import Create, Rotate, Transform, Uncreate, Write
+from manim import FadeIn, FadeOut, FadeTransform, TransformFromCopy
+from manim import FunctionGraph, VGroup
+from manim import Text, Tex
+
+from manim import config
+
+from manim import LEFT, RIGHT, UP, DOWN, PI, DR, DL, UR, UL, LIGHT
+
+# COLORS
+BLUE = "#B0E1FA"
+VIOLET = "#E8C9FA"
+RED = "#F79BC5"
+GREEN = "#DBF9E7"
+YELLOW = "#EFE9B7"
+ORANGE = "#F6CCB0"
+BLACK = "#000000"
+WHITE = "#F4EDDE"
+
+# Make it vertical
+SCALE_FACTOR = 1
+# Flip width => height, height => width
+tmp_pixel_height = config.pixel_height
+config.pixel_height = config.pixel_width
+config.pixel_width = tmp_pixel_height
+# Change coord system dimensions
+config.frame_height = config.frame_height / SCALE_FACTOR
+config.frame_width = config.frame_height * 9 / 16
+
+
+class RotateAndColor(Rotate, Transform):
+    def __init__(
+        self,
+        mobject: Mobject,            
+        angle: float,
+        new_color,
+        **kwargs,
+    ) -> None:
+        self.new_color = new_color
+        super().__init__(mobject, angle=angle, **kwargs)
+
+    def create_target(self) -> Mobject:
+        target = self.mobject.copy()
+        target.set_fill(self.new_color)
+        target.rotate(
+            self.angle,
+            axis=self.axis,
+            about_point=self.about_point,
+            about_edge=self.about_edge,
+        )
+        return target
+
+
+class Pythagorean(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        self.camera.frame.save_state()
+
+        txt_copy = Text(
+            r"@chill.maths", font_size=12,
+            font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        ).to_edge(RIGHT + DOWN, buff=0.1)
+        self.add(txt_copy)
+
+        # Introduction text
+        txt_title = [
+            Tex(r"Des identités", font_size=48, color=BLACK),
+            Tex(r"d'arctangentes", font_size=48, color=BLACK)
+        ]
+        txt_title = VGroup(*txt_title).arrange(DOWN).move_to([0, 2, 0])
+
+        txt = [
+            Tex(r"Démonstration", font_size=36, color=BLACK),
+            Tex(r"Rex H. Wu", font_size=28, color=BLACK)
+        ]
+        txt = VGroup(*txt).arrange(DOWN)
+
+        self.add(
+            txt_title,
+            txt,
+        )
+        self.wait(1)
+        self.play(
+            Uncreate(txt_title),
+            Uncreate(txt),
+        )
+        
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
+        # Logo
+        ref = [
+            Tex(r"Proofs without words:", font_size=30, color=BLACK),
+            Tex(r"Exercises in visual thinking", font_size=30, color=BLACK),
+            Tex(r"Roger B. Nelsen (1993), p. 3", font_size=30, color=BLACK)
+        ]
+        ref = VGroup(*ref)\
+            .arrange(DOWN, aligned_edge=LEFT, center=False, buff=0.1)\
+            .move_to([0, 2, 0])
+
+        self.play(Write(ref))
+
+        text = Text(
+            "chill.maths", font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        )
+        # Ajouter un élément mathématique, par exemple une sinusoïde
+        sine_wave = FunctionGraph(
+            lambda x: 0.1 * np.sin(2 * np.pi * x),
+            x_range=[-3, 3],
+            color=BLACK
+        )
+        sine_wave.next_to(text, DOWN, buff=0.2)
+        
+        self.play(
+            FadeIn(text, scale=0.5),
+            Create(sine_wave),
+            run_time=2
+        )
+
+        self.wait(1)
+
+        
