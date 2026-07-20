3a5bca7bb00b8264f7c71468e50834f014b6a4fd
StevenGolovkine <steven_golovkine@icloud.com>
2026-07-20 10:25:18 +0200

    Hexagonal = triangular

---
+++ Nelsen_I/p106_hexagonal_triangular.py
@@ -0,0 +1,107 @@
+"""
+Visual proof of every hexagonal number is a triangular number.
+Proofs without Words. Roger B. Nelsen. p. 106.
+"""
+import numpy as np
+
+from manim import ThreeDScene
+from manim import Create, Uncreate, Write
+from manim import VGroup, FadeIn, FadeOut, FunctionGraph, Transform
+from manim import MathTable, Brace, RoundedRectangle
+from manim import Arrow, Cube, Line, Text, Tex
+
+from manim import config
+from manim import DEGREES, LEFT, RIGHT, DOWN, LIGHT, UP, PI
+
+# COLORS
+BLUE = "#B0E1FA"
+VIOLET = "#E8C9FA"
+RED = "#F79BC5"
+GREEN = "#DBF9E7"
+YELLOW = "#EFE9B7"
+ORANGE = "#F6CCB0"
+BLACK = "#000000"
+WHITE = "#F4EDDE"
+
+# Make it vertical
+SCALE_FACTOR = 1
+# Flip width => height, height => width
+tmp_pixel_height = config.pixel_height
+config.pixel_height = config.pixel_width
+config.pixel_width = tmp_pixel_height
+# Change coord system dimensions
+config.frame_height = config.frame_height / SCALE_FACTOR
+config.frame_width = config.frame_height * 9 / 16
+
+
+class HexagonalTriangular(ThreeDScene):
+    def construct(self):
+        self.camera.background_color = WHITE
+
+        txt_copy = Text(
+            r"@chill.maths", font_size=12,
+            font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        ).to_edge(RIGHT + DOWN, buff=0.1)
+        self.add(txt_copy)
+        self.add_fixed_in_frame_mobjects(txt_copy)
+
+        # Introduction text
+        txt_title = [
+            Tex(r"Tout nombre", font_size=48, color=BLACK),
+            Tex(r"hexagonal", font_size=48, color=BLACK),
+            Tex(r"est un nombre", font_size=48, color=BLACK),
+            Tex(r"triangulaire.", font_size=48, color=BLACK),
+        ]
+        txt_title = VGroup(*txt_title).arrange(DOWN).move_to([0, 2, 0])
+
+        txt = [
+            Tex(r"Démonstration", font_size=36, color=BLACK),
+            Tex(r"Roger B. Nelsen", font_size=28, color=BLACK)
+        ]
+        txt = VGroup(*txt).arrange(DOWN)
+
+        self.add(
+            txt_title,
+            txt
+        )
+        self.wait(1)
+        self.play(
+            Uncreate(txt_title),
+            Uncreate(txt)
+        )
+
+
+
+        # Finish
+        self.wait(2)
+        self.play(*[FadeOut(mob)for mob in self.mobjects])
+
+        # Logo
+        ref = [
+            Tex(r"Richard K. Guy,", font_size=26, color=BLACK),
+            Tex(r"written communication.", font_size=26, color=BLACK),
+        ]
+        ref = VGroup(*ref)\
+            .arrange(DOWN, aligned_edge=LEFT, center=False, buff=0.1)\
+            .move_to([0, 2, 0])
+
+        self.play(Write(ref))
+
+        text = Text(
+            "chill.maths", font="CMU Typewriter Text", weight=LIGHT, color=BLACK
+        )
+        # Ajouter un élément mathématique, par exemple une sinusoïde
+        sine_wave = FunctionGraph(
+            lambda x: 0.1 * np.sin(2 * np.pi * x),
+            x_range=[-3, 3],
+            color=BLACK
+        )
+        sine_wave.next_to(text, DOWN, buff=0.2)
+        
+        self.play(
+            FadeIn(text, scale=0.5),
+            Create(sine_wave),
+            run_time=2
+        )
+
+        self.wait(1)
